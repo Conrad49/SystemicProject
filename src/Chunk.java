5d8d1cc35c3eb7d301fb72a6d3b8ab35ed47addf
Connor <codingconrad49@gmail.com>
2020-05-09 00:50:57 -0400

    Chunk class

---
+++ src/Chunk.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Chunk {
+
+    ArrayList<Entity> entities;
+    ArrayList<Tile> tiles;
+
+    public Chunk() {
+
+    }
+
+    public void tick (){
+        // tick all things that need to be ticked
+        // include ticking tiles eventually
+    }
+
+}
