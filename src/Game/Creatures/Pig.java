6f93e55ce1cb4d1e470fda96b398eb399d57d087
Lauren <lbsatt326@gmail.com>
2020-05-10 22:12:01 -0400

    Pig

--- src/Game/Creatures/Pig.java
+++ src/Game/Creatures/Pig.java
@@ -8,7 +8,13 @@ import javafx.scene.shape.Rectangle;
  * @author Lauren Sattesahn
  */
 public class Pig extends Entity{
-    public Pig(Color backColor, Rectangle boundsBox) {
-        super(backColor, boundsBox);
+    public Pig(Color backColor, int x, int y) {
+        super(backColor, new Rectangle(x, y, 128, 64));
+        width = (int) boundsBox.getWidth();
+        height = (int) boundsBox.getHeight();
+        Rectangle r = new Rectangle(x, y, width, height);
+
     }
+
+
 }
--- src/Game/Entity.java
+++ src/Game/Entity.java
@@ -12,12 +12,14 @@ public abstract class Entity {
     double ySpeed;
     Image texture;
     Color backColor;
-    Rectangle boundsBox;
+    protected Rectangle boundsBox;
     int tileX;
     int tileY;
     double posX;
     double posY;
     boolean isVisible = false;
+    protected int width;
+    protected int height;
 
     public Entity(Color backColor, Rectangle boundsBox) {
         this.backColor = backColor;
