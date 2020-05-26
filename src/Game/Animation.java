46a91c17db8b4dd02b3cbd602933979f7ddd1bbb
Connor <codingconrad49@gmail.com>
2020-05-26 17:34:53 -0400

    animation testing

---
+++ src/Game/Animation.java
@@ -0,0 +1,34 @@
+package Game;
+
+import javafx.scene.image.Image;
+
+public class Animation {
+    Image[] animation;
+    int count = -1;
+
+    public Animation() {
+
+    }
+
+    public Animation(Image[] animation) {
+        this.animation = animation;
+    }
+
+    public void resetCount(){
+        this.count = -1;
+    }
+
+    public Image getImage(){
+        this.count ++;
+        if (this.count < this.animation.length) {
+            return this.animation[count];
+        } else {
+            this.count = -1;
+            return  this.animation[count];
+        }
+    }
+
+    public void setImages(Image [] images){
+        this.animation = images;
+    }
+}
--- src/Game/Entities/Player.java
+++ src/Game/Entities/Player.java
@@ -1,17 +1,20 @@
 package Game.Entities;
 
+import Game.Animation;
 import Game.Chunk;
 import Game.Main;
 import Game.Tiles.Tile;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
+import java.sql.SQLOutput;
 import java.util.HashSet;
 
 public class Player extends Entity {
 
     Tile[][] visibleTiles = new Tile[10][10];
     private static int speed = 5;
+    private static boolean moving;
 
     private static HashSet<String> currentlyActiveKeys = new HashSet<String>(1);
     private boolean hasChangedFullscreen;
@@ -25,34 +28,65 @@ public class Player extends Entity {
         super(texture, boundsBox);
     }
 
+    public Animation walkDown = new Animation();
+    private Animation walkUp;
+    private Animation walkLeft;
+    private Animation walkRight;
+
+    private Animation currentAnimation;
+
     @Override
     public void tick() {
         super.tick();
+        moving = false;
 
+        this.currentAnimation = this.walkDown;
 
         if (currentlyActiveKeys.contains("A")) {
+            this.currentAnimation = this.walkDown;
+
             this.xSpeed = speed * -1;
             this.posX += this.xSpeed;
             this.tileX = (int)this.posX / Tile.getWidth();
+            moving = true;
+        } else {
+            this.walkDown.resetCount();
         }
 
         if (currentlyActiveKeys.contains("D")) {
+            this.currentAnimation = walkDown;
+
             this.xSpeed = speed;
             this.posX += this.xSpeed;
             this.tileX = (int)this.posX / Tile.getWidth();
+            moving = true;
+        } else {
+            this.walkDown.resetCount();
         }
 
         if (currentlyActiveKeys.contains("W")) {
+            this.currentAnimation = walkDown;
+
             this.ySpeed = speed * -1;
             this.posY += this.ySpeed;
             this.tileY = (int)this.posY / Tile.getWidth();
+            moving = true;
+        } else {
+            this.walkDown.resetCount();
         }
 
         if (currentlyActiveKeys.contains("S")) {
+            this.currentAnimation = walkDown;
+
             this.ySpeed = speed;
             this.posY += this.ySpeed;
             this.tileY = (int)this.posY / Tile.getWidth();
+            moving = true;
+        } else {
+            this.walkDown.resetCount();
         }
+
+
         // Binding for going in and out of fullscreen
         if(currentlyActiveKeys.contains("F11")) {
             if (!hasChangedFullscreen) {
@@ -60,6 +94,8 @@ public class Player extends Entity {
             }
             hasChangedFullscreen = true;
         }
+
+        System.out.println(moving);
 
     }
 
@@ -81,9 +117,14 @@ public class Player extends Entity {
 
     public void setTexture(Image image){
         texture = image;
+        Image[] walkDown = new Image[2];
+        walkDown[0] = image;
+        walkDown[1] = new Image("/res/player.png");
+        this.walkDown.setImages(walkDown);
     }
 
     public Image getTexture(){
-        return texture;
+        return currentAnimation.getImage();
+        //return texture;
     }
 }
