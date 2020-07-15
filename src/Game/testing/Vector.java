140c25b044bfc5cc3ae6a9e5d0b5e5fbbf6b128e
Connor <codingconrad49@gmail.com>
2020-07-14 22:11:25 -0400

    minor fix in the constructor of Vector class

--- src/Game/testing/Vector.java
+++ src/Game/testing/Vector.java
@@ -3,7 +3,7 @@ package Game.testing;
 public class Vector {
     public double x, y;
 
-    public Vector(int x, int y) {
+    public Vector(double x, double y) {
         this.x = x;
         this.y = y;
     }
